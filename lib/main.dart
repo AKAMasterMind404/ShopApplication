6fe56d96f208c45872538e034110d645de6dcb89
AKAMasterMind404 <atharvkarbhari123@gmail.com>
2021-06-14 20:22:44 +0530

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:state_management/views/Home.dart';
 
 void main()=>runApp(
     MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
--- lib/models/product_item.dart
+++ lib/models/product_item.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
@@ -7,25 +5,39 @@ class ProductItem extends StatelessWidget {
   String title;
   String imageUrl;
 
-  ProductItem({Key key, this.id, this.title, this.imageUrl,}) : super(key: key);
+  ProductItem({
+    Key key,
+    this.id,
+    this.title,
+    this.imageUrl,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        title: Text(title, textAlign: TextAlign.center),
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.favorite_border_rounded, color: Colors.pinkAccent),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.shopping_cart),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(title, textAlign: TextAlign.center),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+                Icons.favorite_border_rounded,
+                color: Theme.of(context).accentColor
+            ),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).accentColor,
+            ),
+          ),
         ),
       ),
     );
